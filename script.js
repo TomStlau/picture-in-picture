b5bc2fac74367a0cb6bb50219022fc12e074d8f0
TomStlau <tommy.stlau@gmail.com>
2023-10-14 16:53:06 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById('video')
+const buttonElement = document.getElementById('button')
+
+async function selectMediaStream () {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+    videoElement.srcObject = mediaStream
+    videoElement.onloadedmetadata = () => {
+      videoElement.play()
+    }
+  } catch (error) {
+    console.log('whoops', error)
+  }
+}
+
+buttonElement.addEventListener('click', async () => {
+  buttonElement.disabled = true
+  await videoElement.requestPictureInPicture()
+  buttonElement.disabled = false
+})
+
+selectMediaStream()
